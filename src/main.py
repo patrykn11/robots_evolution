f3eeb2ceeb087fda605ce0df49bee02edab5f9b0
Marcin <wojdalski.marcin2137@gmail.com>
2025-12-27 03:12:31 +0100

    Adding requirements + minor fixes

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from default_genetic_algorithm.genetic_algorithm import GeneticAlgorithm
 from species_genetic_algorithm.genetic_algorithm import SpeciesGeneticAlgorithm
 from controller import Controller
 from species_genetic_algorithm.species import Species
+
+
 if __name__ == "__main__":
     ga = SpeciesGeneticAlgorithm()
     winner = ga.run()
